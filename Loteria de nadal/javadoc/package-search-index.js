c1de580f067b9301aaff08518802c4d18089b02b
osczapmar <121022918+osczapmar@users.noreply.github.com>
2023-02-01 09:29:30 +0100

    ADD JAVADOC

---
+++ Loteria de nadal/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"loteria.de.nadal"},{"l":"utils"}]
---
+++ Loteria de nadal/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"loteria.de.nadal","l":"LoteriaDeNadal"},{"p":"loteria.de.nadal","l":"LoteriaDeNadal.NumPremiado"},{"p":"utils","l":"Utils"}]
